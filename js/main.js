56153ba0c5ac77565db96fac85452e736f69df0f
noliw <noliw2017@gmail.com>
2022-07-07 11:18:10 -0700

    finalizing

--- js/main.js
+++ js/main.js
@@ -7,8 +7,8 @@ const COLORS = {
 
 /*----- app's state (constiables) -----*/
 let board;//0: empty slots, plyr1 or plyr2 for players
-let gameStatus; // 0: game in play, 1 or -1: winner; T for tie
 let turn; // plyr1 or plyr2
+let isGameOver;
 
 /*----- cached element references -----*/
 const markerEls = [...document.querySelectorAll('#markers > div')];//creates an array and spreads it out;
@@ -16,29 +16,36 @@ const markerEls = [...document.querySelectorAll('#markers > div')];//creates an 
 /*----- event listeners -----*/
 // document.querySelectorAll('.board').addEventListener('click', handleClick)
 document.getElementById('markers').addEventListener('click', handleDrop);
+document.getElementById('playAgain').addEventListener('click', playAgain);
+const message = document.getElementById('message')
 /*----- functions -----*/
 init();
-
 
 
 function init() {
   // create a new empty array and fill sets it to 0
   board = [
+    // [col 0, row 3]
     [0, 0, 0, 0, 0, 0], // col0
     [0, 0, 0, 0, 0, 0], // col1
     [0, 0, 0, 0, 0, 0], // col2
     [0, 0, 0, 0, 0, 0], // col3
     [0, 0, 0, 0, 0, 0], // col4
     [0, 0, 0, 0, 0, 0], // col5
-    [0, 0, 0, 0, 0, 0] // col6
+    [0, 0, 0, 0, 0, 0], // col6
   ];
   // game inplay
   // gameStatus = 0;
   // plyr 1 turn
   turn = 1;
+  isGameOver = false;
   render();
 }
 
+function playAgain() {
+  init();
+   message.innerText = '';
+}
 
 // render shows the output in the browser 
 // its job is to transfer/visualize all state to the Dom
@@ -60,65 +67,136 @@ function renderMarkers() {
     markerEl.style.visibility = board[colIdx].includes(0) ? 'visible' : 'hidden';
   });
 }
-// Update all impacted state, then call render
-function handleDrop(evt) {
-  const colIdx = markerEls.indexOf(evt.target);
-  if (colIdx === -1) return;
+
+// update all impacted state then call render()
+function handleDrop(event) {
+  if (isGameOver) {
+    return
+  }
+
+  const colIdx = markerEls.indexOf(event.target);
+  if(colIdx === -1) return;
   const colArr = board[colIdx];
   const rowIdx = colArr.indexOf(0);
   colArr[rowIdx] = turn;
-  turn *= -1;
-  winner = getWinner(colIdx, rowIdx);
+
+  // checkWin returns winning player or null
+  const winner = checkWin(colIdx, rowIdx);
+
+  if (winner === 1 || winner === -1) {
+     // we have winning player
+      isGameOver = true;
+   
+    message.innerText = `Player ${winner === 1 ? 'One' : 'Two'} has won!`;
+  } else {
+    // no winnder, check draw
+
+    if (checkDraw()) {
+      isGameOver = true;
+      message.innerText = 'There is a draw!';
+    }
+  }
+  
+  turn = turn * -1
   render();
-}
+} 
 
-
-
-
-/**
- * It checks for a vertical win or a horizontal win.
- * @param colIdx - the column index of the last move
- * @param rowIdx - the row index of the last move
- */
-function getWin(colIdx, rowIdx) {
-  let winner = checkVertWin(colIdx) || checkHorWin(colIdx, rowIdx);
-}
-
-function checkHorWin(colIdx, rowIdx) {
+// checkWin returns the winning player index or null if there is no winner
+function checkWin(colIdx, rowIdx) {
   const player = board[colIdx][rowIdx];
-  // Move colIdx to left most col that = player
-  while (colIdx - 1 >= 0 && board[colIdx - 1][rowIdx] === player) colIdx--;
-  if (colIdx > 3) return null;
-  const sum = board[colIdx][rowIdx] + board[colIdx + 1][rowIdx] + board[colIdx + 2][rowIdx] + board[colIdx + 3][rowIdx];
-  return (Math.abs(sum) === 4) ? board[colIdx][rowIdx] : null;
+  
+  return checkVertWin(colIdx, rowIdx, player) ||
+    checkHorizWin(colIdx, rowIdx, player) || 
+    checkDiagonalRight(colIdx, rowIdx, player) || 
+    checkDiagonalLeft(colIdx, rowIdx, player)
 }
 
-function checkVertWin(colIdx, rowIdx) {
-  /* checking if the row index of the most recent click is less than 3. If it is, then there's no
-  way that there can be a vertical win, so it returns null. */
-  if (rowIdx < 3) return null;
-  /* creating a variable called colArr and setting it equal to the column array at the index of
-  the column that was clicked. */
+ function checkVertWin(colIdx, rowIdx, player) {
   const colArr = board[colIdx];
-  /*  adding the values of the four cells in a column. */
-  const sum = colArr[rowIdx] + colArr[rowIdx - 1] + colArr[rowIdx - 2] + colArr[rowIdx - 3];
-  /*  checking if the sum of the values of the four cells in a column is 4. If it is, then the
-  value of the first cell is returned. Otherwise, null is returned. */
-  return (Math.abs(sum) === 4) ? colArr[rowIdx] : null;
+  let count = 1;
+  // We can use/modify rowIdx because we won't need
+  // to access it's original value anymore
+  rowIdx--;
+  // Count until no longer the same 'player'
+  while(colArr[rowIdx] === player && rowIdx >= 0) {
+      count++;
+      rowIdx--;
+  }
+  return count === 4 ? player : null;
 }
 
-/*-- END: New approach based upon most recent click --*/
-/**
- * We loop through the board array, and for each column array, we check if there's a winner in that
- * column
- * @returns The winner of the game.
- */
-function getWinner() {
-  let winner;
-  board.some((colArr, colIdx) => {
-    winner = checkCol(colArr, colIdx);
-    return winner;
-  });
-  return winner;
+ function checkHorizWin(colIdx, rowIdx, player) {
+  let idx = colIdx + 1;
+  let count = 1;
+
+  // check if player won from left to right
+  // Count until no longer the same 'player'
+  while(idx < board.length && board[idx][rowIdx] === player) {
+      count++;
+      idx++;
+  }
+
+  if (count === 4) {
+    return player
+  } 
+
+  // check if player won from right to left 
+  idx = colIdx - 1;
+  while(idx >= 0 && board[idx][rowIdx] === player) {
+      count++;
+      idx--;
+  }
+
+  if (count === 4) {
+    return player
+  } 
+   
+  return null;
 }
 
+ function checkDiagonalRight(colIdx, rowIdx, player) {
+   let count = 1;
+
+   let colCounter = colIdx + 1;
+   let rowCounter = rowIdx - 1;
+
+   while(colCounter < cols && rowCounter >= 0 && board[colCounter][rowCounter] === player) {
+     colCounter++;
+     rowCounter--;
+     count++;
+   }
+
+   if (count === 4) {
+    return player;
+   }
+
+   return null; 
+ }
+
+ function checkDiagonalLeft(colIdx, rowIdx, player) {
+   let count = 1;
+
+   let colCounter = colIdx - 1;
+   let rowCounter = rowIdx - 1;
+
+   while(colCounter >= 0 && rowCounter >= 0 && board[colCounter][rowCounter] === player) {
+     colCounter--;
+     rowCounter--;
+     count++;
+   }
+
+   if (count === 4) {
+    return player;
+   }
+
+   return null; 
+ }
+
+ function checkDraw() {
+   // if we find one row that has an empty cell, game is not finished, some player can still make a move...
+   // else, if all the cells are occuppied by some player, and all the other checks fails, then we can consider it a draw, "every cell is filled in before we found the winner"
+   
+   return !board.some(function(cols) {
+    return cols.some(row => row === 0)
+   })
+ } 
